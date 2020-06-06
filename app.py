e4192e8fc283e974baa8a8b6011073c16ede9e0b
Nathaniel Cherian <ngcherian2014@gmail.com>
2020-06-05 18:46:09 -0700

    asthetic

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def humi():
     result = read_dht11_dat()
     if result:
         humidity, temperature = result
-        return render_template('humiture.html', humidity=humidity, temperature=temperature)
+        return render_template('humiture.html', humidity=str(humidity) + '%', temperature=str(temperature) + '°C')
     else:
         return render_template('humiture.html', humidity='N/A', temperature='N/A')
     return 'this is a test'
